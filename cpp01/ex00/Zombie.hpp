0c75ad2f2267bc65c0c7046f959516f1924b75ed
M-elhouary <mohamedelhouary056@gmail.com>
2026-01-06 12:01:45 +0100

    add file structure cpp01

---
+++ cpp01/ex00/Zombie.hpp
@@ -0,0 +1,13 @@
+#ifndef ZOMBIE_HPP
+#define ZOMBIE_HPP
+
+
+#include <string>
+#include <iostream> 
+
+
+
+
+
+
+#endif
---
+++ cpp01/ex00/main.cpp
@@ -0,0 +1,9 @@
+#include    "Zombie.hpp"
+
+
+
+int main()
+{
+
+    return 0;
+}
