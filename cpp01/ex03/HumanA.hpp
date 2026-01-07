d886a089e4b16f76cff9d2ca5645affee087249d
M-elhouary <mohamedelhouary056@gmail.com>
2026-01-07 13:36:22 +0100

    add class && makefile of ex03

--- cpp01/ex03/HumanA.hpp
+++ cpp01/ex03/HumanA.hpp
@@ -1,1 +1,20 @@
+#ifndef HUMANA_HPP
+#define HUMANA_HPP
 
+#include <iostream>
+#include "Weapon.hpp"
+
+
+class HumanA{
+    private:
+        std::string name;
+        Weapon      *weapon;
+
+    public:
+        HumanA(std::string name);
+        setWeapon(Weapon &weapon);
+        void attack();
+
+};
+
+#endif
--- cpp01/ex03/Weapon.hpp
+++ cpp01/ex03/Weapon.hpp
@@ -1,1 +1,20 @@
+#ifndef WEAPON_HPP
+#define WEAPON_HPP
 
+
+#include <iostream>
+
+class Weapon{
+    private:
+        std::string type;
+    public:
+        Weapon();
+        ~Weapon();
+        void getType(std::string name);
+        void setType(std::string name);
+
+};
+
+
+
+#endif
